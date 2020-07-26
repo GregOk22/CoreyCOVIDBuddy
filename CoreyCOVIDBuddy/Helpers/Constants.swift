bc18d34eaab0cabc3bb290ec1e6b1c04b8fa7f4d
Corey-COVIDBuddy <gregok.22@gmail.com>
2020-07-26 10:36:18 -0400

    Corey: COVID Buddy added to GH

---
+++ CoreyCOVIDBuddy/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CoreyCOVIDBuddy
+//
+//  Copyright © 2020 Gregory Okhuereigbe. All rights reserved.
+//
+
+import Foundation
+
+struct Constants
+{
+    struct Storyboard
+    {
+        static let homeViewController = "WelcomeVC"
+    }
+}
---
+++ Pods/Target Support Files/Pods-AnimArenaUITests/Pods-AnimArenaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnimArenaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AnimArenaUITests
+@end
